63cfd79c4014544bddc40d70171e46fc63511c49
maxim <max20042011@gmail.com>
2023-10-30 17:56:54 +0300

    a

--- labfinal.py
+++ labfinal.py
@@ -1,6 +1,7 @@
 
 import csv
 import copy
+from timeit import timeit
 from qsort import *
 from timsort import *
 from datetime import datetime
@@ -31,10 +32,54 @@ def readArray(fname):
     numArr = [int(x) for x in rdrArr]
     return numArr
 
-def main():
-    #writeArrays()
+def qsort_time(arr):
+    QS_array = copy.deepcopy(arr) #глубокая копия превентивная мера ассоциации двух массивов с одним отрезком памяти
+    start_time = datetime.now()
+    qsort(QS_array)
+    end_time = datetime.now()
+    exec_delta = end_time - start_time 
+    print("Quicksort exec took: ", exec_delta)
 
-    #
+def timsort_time(arr):
+    TS_array = copy.deepcopy(arr) #глубокая копия превентивная мера ассоциации двух массивов с одним отрезком памяти
+    start_time = datetime.now()
+    timsort(TS_array)
+    end_time = datetime.now()
+    exec_delta = end_time - start_time
+    print("Timsort exec took: ", exec_delta)
+    
+
+
+
+merged_array = readArray("f1.txt") + readArray("f2.txt")
+def main():
+#3.2.1. Внешняя сортировка. 
+    #3.2.1.1 объединение
+    print("сортировка продукта объединения массивов")
+
+    #merged_array += (readArray("f1.txt")) 
+    #merged_array += (readArray("f2.txt")) 
+
+    print(timeit('timsort(merged_array)', number=1, globals = globals()))
+    #qsort_time(merged_array)
+    #timsort_time(merged_array)
+    print("\n")
+
+    #3.2.1.2 перечисление
+    print("сортировка продукта перечисления массивов")
+
+    #intersection_array = []
+    #intersection_array = list(set(readArray("f1.txt")).intersection(set(readArray("f2.txt"))))
+
+    #print(intersection_array)
+
+    #qsort_time(intersection_array)
+    #timsort_time(intersection_array)
+    print("\n")
+
+    #3.2.1.3 разность
+
+    '''
     TS_array = []
     TS_array += (readArray("f1.txt")) 
     TS_array += (readArray("f2.txt")) 
@@ -49,15 +94,6 @@ def main():
     QS_array += (readArray("f1.txt")) 
     QS_array += (readArray("f2.txt")) 
 
-
-    QS_start_time = datetime.now()
-    qsort(QS_array)
-    QS_end_time = datetime.now()
-    print("Quicksort exec took: {}".format(QS_end_time -  QS_start_time))
-
-    
-
-
-
+'''
 main()
     
